3539cd58fce3a43f51169bcd07518dd6b173c954
suppanutpw <suppanut_2540@hotmail.com>
2022-05-07 11:53:57 +0700

    refactoring code + add comment

--- 62070186-netmiko.py
+++ 62070186-netmiko.py
@@ -2,9 +2,11 @@ import textfsm
 from jinja2 import Template
 from netmiko import ConnectHandler
 
+# import jinja2 file template
 template_file = open('manage_loopback.j2', 'r').read()
 jinja_template = Template(template_file)
 
+# device config to create ssh connect
 device_params = {
   'device_type': 'cisco_ios',
   'ip': '10.0.15.113',
@@ -14,14 +16,17 @@ device_params = {
 
 with ConnectHandler(**device_params) as ssh:
 
+    # default method will be create loopback interface
     method = 'create'
     with open('cisco_ios_show_interfaces.textfsm') as template_file:
         interface = ssh.send_command('show interfaces L62070186')
         textfsm_template = textfsm.TextFSM(template_file)
         interface = textfsm_template.ParseText(interface)
         if interface:
+            # if there already have interface, will change method to delete interface
             method = 'delete'
 
+    # generate set of command from jinja2
     commands = jinja_template.render(
         method = method,
         interface = 'Loopback 62070186',
@@ -29,6 +34,7 @@ with ConnectHandler(**device_params) as ssh:
         subnet = '255.255.255.0'
     ).splitlines()
 
+    # clean code for easy to read on output terminal
     commands = [ command.strip() for command in commands if command.strip() != '' ]
-    print(ssh.send_config_set(commands))
-    print(ssh.save_config())
+    print(ssh.send_config_set(commands)) # send list of commands from jinja2
+    print(ssh.save_config()) # saving running configuration
