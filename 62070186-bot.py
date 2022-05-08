1b1529eb08d34ea320e5eda4486677a0879ba7f1
suppanutpw <suppanut_2540@hotmail.com>
2022-05-08 16:07:35 +0700

    change data post data type to json

--- 62070186-bot.py
+++ 62070186-bot.py
@@ -1,24 +1,31 @@
 import time
 import json
 import requests
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 requests.packages.urllib3.disable_warnings()
 
 student_code = '62070186'
 interface_status_api_url = f'https://10.0.15.113/restconf/data/ietf-interfaces:interfaces-state/interface=Loopback{student_code}/oper-status'
 
-webex_room = 'NPA2021@ITKMITL'
+webex_room = 'NPA2021@ITKMITL' # change room name here for testing
 webex_apt_url = 'https://webexapis.com'
-webex_api_token = 'webex-api-token-here' # hide webex api token for security
+webex_api_token = 'webex-api-token-here' # please use your api token
+
+# RESTCONF to CSR1000v packet header
+device_headers = {
+    'Accept': 'application/yang-data+json', 
+    'Content-type':'application/yang-data+json'
+}
+basicauth = ('admin', 'cisco')
+
+# WebEX RestAPI Service packet header
+webex_headers = {
+    'Authorization': 'Bearer ' + webex_api_token,
+    'Content-Type': 'application/json'
+}
 
 def check_interface_status():
     """ check interface status when lastest message is student_code """
-    headers = {
-        'Accept': 'application/yang-data+json', 
-        'Content-type':'application/yang-data+json'
-    }
-    basicauth = ('admin', 'cisco')
-    response = requests.get(interface_status_api_url, auth=basicauth, headers=headers, verify=False)
+    response = requests.get(interface_status_api_url, auth=basicauth, headers=device_headers, verify=False)
 
     if not response.ok:
         print(f'Error Status Code ({response.status_code}): Device api url error or interface not found')
@@ -30,15 +37,11 @@ def check_interface_status():
 def webex_post_message(webex_room_id, text):
     """ post interface status to webex room """
     webex_post_message_url = webex_apt_url + '/v1/messages'
-    msg = MultipartEncoder({
+    msg = {
         'roomId': webex_room_id,
         'text': text
-    })
-    headers = {
-        'Authorization': f'Bearer {webex_api_token}',
-        'Content-Type': msg.content_type,
     }
-    response = requests.post(webex_post_message_url, data=msg, headers=headers, verify=False)
+    response = requests.post(webex_post_message_url, headers=webex_headers, json=msg)
 
     if not response.ok:
         print(f'Error Status Code ({response.status_code}): {json.dumps(response.json(), indent=4)}')
@@ -46,11 +49,7 @@ def webex_post_message(webex_room_id, text):
 def webex_find_room():
     """ find room id via room name """
     webex_list_rooms_url = webex_apt_url + '/v1/rooms'
-    headers = {
-        'Authorization': f'Bearer {webex_api_token}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(webex_list_rooms_url, headers=headers)
+    response = requests.get(webex_list_rooms_url, headers=webex_headers)
     if not response.ok:
         print(f'Error Status Code ({response.status_code}): {json.dumps(response.json(), indent=4)}')
         return None
@@ -64,11 +63,7 @@ def webex_find_room():
 def check_lastest_message(webex_room_id, webex_lastest_message_id):
     """ get lastest message and check that message is not query yet """
     webex_lastest_message_url = webex_apt_url + f'/v1/messages?roomId={webex_room_id}&max=1'
-    headers = {
-        'Authorization': f'Bearer {webex_api_token}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(webex_lastest_message_url, headers=headers)
+    response = requests.get(webex_lastest_message_url, headers=webex_headers)
     if not response.ok:
         print(f'Error Status Code ({response.status_code}): {json.dumps(response.json(), indent=4)}')
         return ''
@@ -87,12 +82,11 @@ def check_lastest_message(webex_room_id, webex_lastest_message_id):
 def main():
     """ main code here """
     webex_room_id = webex_find_room()
+    webex_lastest_message_id = ''
 
     if not webex_room_id:
-        print('Error: Webex Room Not Found')
-        return
+        exit('Error: Webex Room Not Found')
 
-    webex_lastest_message_id = ''
     while True:
         webex_lastest_message_id = check_lastest_message(webex_room_id, webex_lastest_message_id)
         time.sleep(1)
